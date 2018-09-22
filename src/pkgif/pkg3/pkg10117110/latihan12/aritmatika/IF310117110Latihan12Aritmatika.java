3c1ee918746c9cb159653b769d3624b8c6d82334
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:16:54 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan12/aritmatika/IF310117110Latihan12Aritmatika.java
@@ -0,0 +1,33 @@
+
+package pkgif.pkg3.pkg10117110.latihan12.aritmatika;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Operator Aritmatika +,-,/* dll
+ */
+public class IF310117110Latihan12Aritmatika {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("a-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+
+    }
+    
+}
